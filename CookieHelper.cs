543ebe92a74e751a3c9e33ce586416492220326d
Yunus Emre ÜNAL <yunusemreunal@gmail.com>
2017-03-19 13:45:50 +0200

    Add files via upload

---
+++ CookieHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web;
+
+namespace CodeForApp.Core.Helpers
+{
+    public static class CookieHelper
+    {
+        public static void SetCookie(string name, string value)
+        {
+            var myCookie = new HttpCookie(name)
+                           {
+                               Value = HttpContext.Current.Server.UrlEncode(value),
+                               Expires = DateTime.Now.AddMonths(1)
+                           };
+            HttpContext.Current.Response.Cookies.Add(myCookie);
+        }
+
+        public static string GetCookie(string name)
+        {
+            var httpCookie = HttpContext.Current.Request.Cookies[name];
+            return httpCookie != null ? HttpContext.Current.Server.UrlDecode(httpCookie.Value) : "0";
+        }
+
+        public static void DeleteCookie(string name)
+        {
+            var currentUserCookie = HttpContext.Current.Request.Cookies[name];
+            HttpContext.Current.Response.Cookies.Remove(name);
+
+            if (currentUserCookie == null) return;
+
+            currentUserCookie.Expires = DateTime.Now.AddDays(-10);
+            currentUserCookie.Value = null;
+            HttpContext.Current.Response.SetCookie(currentUserCookie);
+        }
+    }
+}
